677ac0b6cbda8b9cdb923b6242aaebc7c1496e62
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-12-28 18:47:07 +0400

    feat: api path redirect all resolve

--- bin/cart-integration-service.ts
+++ bin/cart-integration-service.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigatewayv2';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
@@ -10,43 +10,44 @@ const app = new cdk.App();
 
 const stack = new cdk.Stack(app, 'CartIntegrationStack');
 
-const nestWrapperLambda = new NodejsFunction(
-  stack,
-  'NestWrapperLambdaSt',
-  {
-    functionName: 'nest-wrapper',
-    runtime: lambda.Runtime.NODEJS_18_X,
-    handler: 'handler',
-    entry: './dist/src/main.js',
-    bundling: {
-      externalModules: [
-        'class-transformer',
-        '@nestjs/microservices',
-        '@nestjs/websockets/socket-module',
-        '@nestjs/microservices/microservices-module',
-        '@nestjs/microservices',
-        'class-validator'
-      ],
-    },
-  }
-);
-
-
-const apiGateway = new apigw.HttpApi(stack, 'CartIntegrationApiGateway', {
-  corsPreflight: {
-    allowHeaders: ['*'],
-    allowOrigins: ['*'],
-    allowMethods: [apigw.CorsHttpMethod.ANY],
+const nestWrapperLambda = new NodejsFunction(stack, 'NestWrapperLambdaSt', {
+  functionName: 'nest-wrapper',
+  runtime: lambda.Runtime.NODEJS_18_X,
+  handler: 'handler',
+  entry: './dist/src/main.js',
+  bundling: {
+    externalModules: [
+      'class-transformer',
+      '@nestjs/microservices',
+      '@nestjs/websockets/socket-module',
+      '@nestjs/microservices/microservices-module',
+      '@nestjs/microservices',
+      'class-validator',
+    ],
   },
 });
 
-apiGateway.addRoutes({
-  integration: new HttpLambdaIntegration(
-    'nestWrapperIntegrationApiGatewaySt',
-    nestWrapperLambda
-  ),
-  path: '/',
-  methods: [apigw.HttpMethod.GET],
-});
+// const apiGateway = new apigw.HttpApi(stack, 'CartIntegrationApiGateway', {
+//   corsPreflight: {
+//     allowHeaders: ['*'],
+//     allowOrigins: ['*'],
+//     allowMethods: [apigw.CorsHttpMethod.ANY],
+//   },
+// });
+
+// apiGateway.addRoutes({
+//   integration: new HttpLambdaIntegration(
+//     'nestWrapperIntegrationApiGatewaySt',
+//     nestWrapperLambda,
+//   ),
+//   path: '/*',
+//   methods: [apigw.HttpMethod.ANY],
+// });
+
+const api = new apigw.RestApi(stack, 'CartIntegrationApi');
+
+const lambdaIntegration = new apigw.LambdaIntegration(nestWrapperLambda);
+
+api.root.addResource("{proxy+}").addMethod("ANY", lambdaIntegration);
 
 app.synth();
